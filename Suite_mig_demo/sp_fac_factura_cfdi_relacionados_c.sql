5f653892f80ae302f267644ba13de7f15bbea97f
JonathanRH07 <45106857+JonathanRH07@users.noreply.github.com>
2020-01-29 11:11:39 -0600

    Modificacion sp de relacionados

--- Suite_mig_demo/sp_fac_factura_cfdi_relacionados_c.sql
+++ Suite_mig_demo/sp_fac_factura_cfdi_relacionados_c.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`%` PROCEDURE `sp_fac_factura_cfdi_relacionados_c`(
+CREATE DEFINER=`icaavweb`@`%` PROCEDURE `sp_fac_factura_cfdi_relacionados_c`(
 	IN	pr_id_factura							INT,
     OUT pr_message								VARCHAR(500)
 )
@@ -24,11 +24,12 @@ BEGIN
 		fac.fecha_factura fecha,
 		fac.total_moneda_base importe,
 		rel.uuid,
-		fac.estatus
+		fac.estatus,
+		rel.tipo_relacion
 	FROM ic_fac_tr_factura fac
-	JOIN ic_fac_tr_factura_cfdi cfdi ON
+	LEFT JOIN ic_fac_tr_factura_cfdi cfdi ON
 		fac.id_factura = cfdi.id_factura
-	JOIN ic_fac_tr_factura_cfdi_relacionados rel ON
+	LEFT JOIN ic_fac_tr_factura_cfdi_relacionados rel ON
 		fac.id_factura = rel.id_factura
 	JOIN ic_cat_tr_serie ser ON
 		fac.id_serie = ser.id_serie
